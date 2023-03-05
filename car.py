3e4f6d12a9b40f535b88604b481d569771461246
Andrew6932 <vivoto35@gmail.com>
2023-03-05 16:05:56 +0200

    Створення функції drift

--- car.py
+++ car.py
@@ -1,9 +1,10 @@
 import math
-
+import random
 import pygame
 
+from constants import FRICTION
 from loader import CAR_IMG
-from screen_update import create_tire_track, blit_rotate, remove_old_tire_tracks
+from screen_update import create_tire_track, blit_rotate
 
 
 class AbstractCar:
@@ -17,21 +18,43 @@ class AbstractCar:
         self.angle = 0
         self.acceleration = 0.1
         self.acceleration_back = 0.05
+        self.drift_angle = 5
+
+    def drift(self, sides):
+        if sides == "left":
+            drift_angle = self.drift_angle * (self.vel / self.max_vel)
+            self.vel *= 0.95
+            self.angle += drift_angle
+            self.x += math.sin(math.radians(self.angle)) * self.vel / 4  # adjust position based on angle and drift
+            self.y += math.cos(math.radians(self.angle)) * self.vel / 4
+        elif sides == "right":
+            drift_angle = self.drift_angle * (self.vel / self.max_vel)
+            self.vel *= 0.95
+            self.angle -= drift_angle
+            self.x -= math.sin(math.radians(self.angle)) * self.vel / 4  # adjust position based on angle and drift
+            self.y -= math.cos(math.radians(self.angle)) * self.vel / 4
+        create_tire_track((self.x, self.y), self.angle, 200)
 
     def rotate(self, left=False, right=False):
         if left:
             self.angle += self.rotation_vel * self.vel / self.max_vel
+
+            self.x += math.sin(math.radians(self.angle)) * self.vel / 6  # adjust position based on angle and drift
+            self.y += math.cos(math.radians(self.angle)) * self.vel / 6
         elif right:
             self.angle -= self.rotation_vel * self.vel / self.max_vel
 
-        if self.vel == self.max_vel:
-            create_tire_track((self.x, self.y), self.angle, 200)
+            self.x -= math.sin(math.radians(self.angle)) * self.vel / 6  # adjust position based on angle and drift
+            self.y -= math.cos(math.radians(self.angle)) * self.vel / 6
 
     def draw(self, screen):
         blit_rotate(screen, self.img, (self.x, self.y), self.angle)
 
-    def move_forward(self):
-        self.vel = min(self.vel + self.acceleration, self.max_vel)
+    def move_forward(self, drift):
+        if drift:
+            self.vel = min(self.vel, self.max_vel)
+        else:
+            self.vel = min(self.vel + self.acceleration, self.max_vel)
         self.move()
 
     def move_backward(self):
